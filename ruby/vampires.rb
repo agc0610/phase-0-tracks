477011e28353cf0ec44e28e4941cc14bf560a2d9
Aimee Gonzalez <agc0610@gmail.com>
2016-10-07 12:19:34 -0700

    4.4 release 1 initial code draft

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+var name = gets.chomp
+
+
+puts "How old are you?"
+var age = gets.chomp
+
+puts "What year were you born?"
+var birthyear = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+var garlicbread = gets.chomp
+
+puts "Would you lke to enroll in the company's health insurance?"
+var insurance = gets.chomp
