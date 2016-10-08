c3ff0442a189042d577090be31b5532b8792585b
Aimee Gonzalez <agc0610@gmail.com>
2016-10-08 16:35:54 -0700

    add encryption method

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,29 @@
+letterbank = "abcdefghijklmnopqrstuvwxyz"
+index = 0
+
+def encrypt (string)
+  letterbank = "abcdefghijklmnopqrstuvwxyz"
+  index = 0
+    while index < string.length do
+      if
+        string[index] == " "
+        string[index] = " "
+        puts string[index]
+      elsif
+        string[index] == "z"
+        string[index] = "a"
+        puts string[index]
+      else
+        string[index] = letterbank[letterbank.index(string[index])+1]
+        puts string[index]
+      end
+
+      index +=1
+
+  end
+end
+
+encrypt("zoo")
+
+
+
