408cb18c3adc0ce9fc85bfbddda0f5b678eff041
Aimee Gonzalez <agc0610@gmail.com>
2016-10-08 16:41:10 -0700

    add decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -25,5 +25,29 @@ end
 
 encrypt("zoo")
 
+def decrypt (string)
+  letterbank = "abcdefghijklmnopqrstuvwxyz"
+  index = 0
+    while index < string.length do
+      if
+        string[index] == " "
+        string[index] = " "
+        puts string[index]
+      elsif
+        string[index] == "z"
+        string[index] = "y"
+        puts string[index]
+      else
+        string[index] = letterbank[letterbank.index(string[index])-1]
+        puts string[index]
+      end
+
+      index +=1
+
+  end
+end
+
+decrypt("app")
+
 
 
