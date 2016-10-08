91e2cd42c805865c190504cd9ff46e61cca05df8
Aimee Gonzalez <agc0610@gmail.com>
2016-10-07 23:06:56 -0700

    edit code formatting for data intake

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,15 +1,21 @@
-puts "What is your name?"
-var name = gets.chomp
+# user input variables
+name = ""
+age = 0
+birthyear = 0
+garlicbread = false
+insurance = false
 
+puts "What is your name?"
+name = gets.chomp
 
 puts "How old are you?"
-var age = gets.chomp
+age = gets.chomp
 
 puts "What year were you born?"
-var birthyear = gets.chomp
+birthyear = gets.chomp
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-var garlicbread = gets.chomp
+garlicbread = gets.chomp
 
-puts "Would you lke to enroll in the company's health insurance?"
-var insurance = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
