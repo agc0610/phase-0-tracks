a6767052c516c0c135fbb7c8c417751854b6afc5
Aimee Gonzalez <agc0610@gmail.com>
2016-10-08 17:06:19 -0700

    add release 4 and 5 of 4.6

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -23,8 +23,6 @@ def encrypt (string)
   end
 end
 
-encrypt("zoo")
-
 def decrypt (string)
   letterbank = "abcdefghijklmnopqrstuvwxyz"
   index = 0
@@ -47,7 +45,24 @@ def decrypt (string)
   end
 end
 
-decrypt("app")
+puts "Would you like to encrypt or decrypt a password?"
+ answer = gets.chomp
+
+ if answer == "encrypt"
+  puts "Please provide your password."
+  encrypt(gets.chomp)
+
+ else answer == "decrypt"
+  puts "Please provide your password."
+  decrypt(gets.chomp)
+
+ end
+
+
+
+#decrypt(encrypt("swordfish"))
+#Release 4: Status update on 10/8 is that we noticed when we run our code, even though it is successful it always ends with a nil line. This nested method is currently breaking because it takes the nil as part of the string for decrypt, and runs it against .length, which is impossible. We are not sure why nil shows up. We briefly discussed chomping off nil, but we probably should just figure out how to not have nil in the first place.
+
 
 
 
