a664f7f1ab1b79f6ab99583fabf806ba1fb4cfae
Aimee Gonzalez <agc0610@gmail.com>
2016-10-10 08:10:16 -0700

    release 2 code so far--got stuck

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,40 +1,41 @@
-# user input variables
-name = ""
-age = 0
-birthyear = 0
-garlicbread = true
-insurance = true
-
+#release 1 interface to gather new employee data
 puts "What is your name?"
 name = gets.chomp
 
 puts "How old are you?"
-age = gets.chomp
+age = gets.to_i
 
 puts "What year were you born?"
-birthyear = gets.chomp
+birthyear = gets.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
 garlicbread = gets.chomp
 
-puts "Would you like to enroll in the company's health insurance?"
+puts "Would you like to enroll in the company's health insurance? (Y/N)"
 insurance = gets.chomp
 
+#release 2 conditionals
+def employeecheck (birth_year, garlic_bread, insurance)
 
-agecorrect = age corresponds to birthyear
-agecorrect = true
+case birth_year
+  when 1900..2000 && (garlic_bread == true || insurance == true)
+  puts "Probably not a vampire"
 
-if agecorrect && garlicbread && insurance
-  puts "Probably not a vampire."
+   when
+   !1900..2000 && (garlic_bread == false || insurance == false)
+   puts "Probably a vampire"
 
+   when
+   !1900..2000 && garlic_bread == false && insurance == false
+   puts "Almost certainly a vampire"
 
-if !agecorrect && !garlicbread || !insurance
-  puts "Probably a vampire."
+   when
+   name == "Drake Cula" || name == "Tu Fang"
+   puts "Definitely a vampire"
 
-if !agecorrect && !garlicbread && !insurance
-  puts "Almost certainly a vampire."
+   else
+   puts "Results inconclusive"
 
-
-
-
-
+   end
+   return
+end
