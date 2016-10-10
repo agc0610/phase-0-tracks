d454f105c6181593cc747bcef95d2e6225ef7cc8
Aimee Gonzalez <agc0610@gmail.com>
2016-10-10 08:15:14 -0700

    release 2 code so far--got stuck

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,4 +1,4 @@
-# gather new employee data
+#release 1 interface to gather new employee data
 
 puts "What is your name?"
 name = gets.chomp
@@ -15,3 +15,28 @@ garlicbread = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
 
+#release 2 conditionals to check for vampire status
+def employeecheck (birth_year, garlic_bread, insurance)
+
+case birth_year
+  when 1900..2000 && (garlic_bread == true || insurance == true)
+  puts "Probably not a vampire"
+
+   when
+   !1900..2000 && (garlic_bread == false || insurance == false)
+   puts "Probably a vampire"
+
+   when
+   !1900..2000 && garlic_bread == false && insurance == false
+   puts "Almost certainly a vampire"
+
+   when
+   name == "Drake Cula" || name == "Tu Fang"
+   puts "Definitely a vampire"
+
+   else
+   puts "Results inconclusive"
+
+   end
+   return
+end
