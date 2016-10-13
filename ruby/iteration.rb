03150b889c2dfb13e484f8fb557d6685dc011636
Aimee Gonzalez <agc0610@gmail.com>
2016-10-12 22:42:42 -0700

    complete release 2 requirements

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -8,6 +8,7 @@
 
 # rainbow_unicorn {|name1, name2| puts "Here come #{name1} and #{name2}"}
 
+
  unicorns = ["Moonshine", "Sprinkles", "Buttercup", "Joe"]
    puts unicorns
    unicorns.each{|show| puts "#{show} is a unicorn!"}
@@ -18,6 +19,11 @@
    end
    puts unicorns
 
+#release 2 examples of specificity
+   # unicorns.delete_if{|unicorn| unicorn == "Buttercup"}
+   # unicorns.keep_if{|unicorn| unicorn == "Moonshine"}
+   # unicorns.select!{|unicorn| unicorn == "Joe"}
+   unicorns.reject!{|unicorn| unicorn["Sprinkles"]}
 
 unicorn_mane_colors = {
   "Moonshine" => "blue sparkle",
@@ -28,3 +34,8 @@ unicorn_mane_colors = {
 
   unicorn_mane_colors.each {|name, color| puts "#{name} has a #{color} mane"}
 
+#release 2 examples of specificity
+  # unicorn_mane_colors.delete_if {|name, color| name == "Joe" || color == "neon green"}
+  # unicorn_mane_colors.keep_if{|name, color| name == "Joe" && color == "very black"}
+  # unicorn_mane_colors.select!{|name, color| name == "Moonshine" || color == "yellow"}
+  unicorn_mane_colors.reject!{|name, color| color == "neon green" || color == "blue sparkle"  }
