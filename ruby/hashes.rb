00e5a562cc10082ed5564be5f026432a482195ea
Aimee Gonzalez <agc0610@gmail.com>
2016-10-14 23:41:57 -0700

    work on release 3 bullet points 3, 4

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,30 +1,50 @@
-# pseudocode and write a program that will allow an interior designer to enter the details of a given client:
+# initiate an empty hash
 
-client_details = {
-:full_name => "",
-:age => "",
-:city => "",
-:state => "",
-:needs_childproofing => "",
-:number_of_children => "",
-:decor_theme => ["nautical", "vintage", "modern", "minimalist"],
-:likes_matching => "",
-:primary_colors_permitted => "",
-:budget => ""
-}
+client_details = {}
 
-# Your program should ...
 #   Prompt the designer/user for all of this information.
-
 #   Convert any user input to the appropriate data type.
+
+  puts "What is your full name?"
+    client_details[:name] = gets.chomp
+  puts "What is your age?"
+    client_details[:age] = gets.to_i
+  puts "What city do you live in?"
+    client_details[:city] = gets.chomp
+  puts "What state? (Sometimes different states have the same city!)"
+    client_details[:state] = gets.chomp
+  puts "Do you need childproofing? Y/N"
+    client_details[:needs_childproofing] = gets.chomp
+  puts "How many children do you have?"
+    client_details[:children] = gets.to_i
+  puts "What decor theme do you prefer?"
+  client_details[:decor_theme] = gets.chomp
+  puts "Do you like your decor to match closely? Y/N"
+    client_details[:matching] = gets.chomp
+  puts "Do you like primary colors? Y/N"
+    client_details[:primary_colors] = gets.chomp
+  puts "What is your budget?"
+    client_details[:budget] = gets.to_i
 
 #   Print the hash back out to the screen when the designer has answered all of the questions.
 
+p client_details
+
+
 #   Give the user the opportunity to update a key (no need to loop, once is fine). After all, sometimes users make mistakes!
+NEED TO OPEN UP TO USER INPUT INSTEAD OF EXITING OUT. DOES SIMPLY HAVING MORE CODE STOP THIS?
 
-#       If the designer says "none", skip it.
+  puts "Any changes to make to the client's answers?"
+    get designers answer
+    store in variable to call in if/else loop
 
-#       But if the designer enters "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key.
+      if the designer says "none"
+        >>skip it
+      elsif the designer enters "decor_theme" (or some other key),
+        >>your program should ask for a new value and update the :decor_theme key.
+
 
 #       (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
+
 #   Print the latest version of the hash, and exit the program.
+
