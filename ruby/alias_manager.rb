e06c8be8e9a191e82e204be02479eeb6871460aa
Aimee Gonzalez <agc0610@gmail.com>
2016-10-16 21:03:40 -0700

    add release 1 and 2

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,7 +1,6 @@
-puts "Welcome to the spy name generator! What is your name?"
-name = gets.chomp
+puts "Welcome to the spy name generator!"
 
-
+def spy_name(name, name_store)
 # pseudocode and write a method that takes a spy's real name (e.g., "Felicia Torres") and creates a fake name with it by doing the following:
 
 # Swap the first and last name.
@@ -9,7 +8,6 @@ name = gets.chomp
 
 #separate the new string into an array of letters
   step_two = step_one.split("")
-
 
 #cycle through the array of letters
 #doing alias method--will go back and try again to do vowels and consonants later
@@ -26,5 +24,36 @@ name = gets.chomp
   step_six = step_five.sort.join
 
   #pronouncing the result is half the mystery...
+  puts "Your spy name is #{step_six}"
 
-  puts "Your agent name is #{step_six}"
+  name_store[name] = step_six
+end
+
+#repeatedly gets a name to work with and sends to the method to operate on
+def get_name
+  puts "What is your name? Type quit to finish"
+  name = gets.chomp
+  return name
+end
+
+def play
+  name_store = {}
+  name = get_name
+  until name == "quit" do
+    spy_name(name, name_store)
+    name = get_name
+  end
+  p name_store
+end
+
+play
+
+
+
+
+
+
+
+
+
+
