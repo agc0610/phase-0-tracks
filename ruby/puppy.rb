a8a935b609c769ee9fc09eee86f82de461c6d597
Aimee Gonzalez <agc0610@gmail.com>
2016-10-19 17:23:22 -0700

    complete release 0, design puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# Species
+#   Puppyus Supercuteus
+
+# Characteristics
+#   Paw count: 4
+#   Color: varies
+#   Name: varies
+#   Barking: varies
+#   Friendly: yes
+#   Tail count: 1
+
+
+# Behavior
+#   Kiss
+#   Prance
+#   Bark
+#   Wriggle
+#   Tug
