3eef2437a2cccfa772aafd1a129aa14166cc625e
Aimee Gonzalez <agc0610@gmail.com>
2016-10-20 23:15:19 -0700

    complete release 4, reflect

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -107,3 +107,19 @@ update_quantity("carrots", 3, grocery_list)
 
 grocery_list = {"carrots"=>3, "apples"=>1, "cereal"=>1, "pizza"=>1, "milk"=>2}
 print_grocery_list(grocery_list)
+
+
+
+#REFLECTION (note: my pair did not show up for this challenge, so I worked solo with my guide there)
+# What did you learn about pseudocode from working on this challenge?
+  #I learned more about getting comfortable with how detailed or high level my pseudocode should be. I struggled to not stray into coding itself before it was time, and this forced me to practice.
+# What are the tradeoffs of using arrays and hashes for this challenge?
+  #Arrays are more appropriate if you have only grocery items in your list. Since we also wanted to work with quantities, we had pairs of data which is a cue to use a hash and store that data in a key/value pair.
+# What does a method return?
+  #A method returns the calculated result of a series of operations on data that is passed into the method.
+# What kind of things can you pass into methods as arguments?
+  #Based on this lesson, I saw that you can pass in strings, integers, and whole hashes. I assume that one can also pass in an array.
+# How can you pass information between methods?
+  #In this lesson after I created a hash, I could pass it into the argument list that any other method took. I think depending on the situation, one might create a new variable like updated_grocery_list and then store the output of the current method there so it can be called later, but I'm not sure if that is superfluous and makes the code heavier.
+# What concepts were solidified in this challenge, and what concepts are still confusing?
+  #I feel much more comfortable identifying the pieces of a method and determining which things I can do with which class (hash versus array versus string) based on the documentation. I think I am still murky on how grocery_list is passed to the various subsequent methods, because I hard-coded the new grocery_list hash for each one to call, but when I tried running methods without this definition, it gave me errors about variable definitions not existing.
