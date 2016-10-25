9ed4c441369d115609c63b6becd38e4fd2e43200
Aimee Gonzalez <agc0610@gmail.com>
2016-10-24 17:47:22 -0700

    complete release 4

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -32,7 +32,7 @@ class Santa
     #get its index position in the array
     #use that index position to insert it at the end; fixed array of length 7 b/c there are 8 reindeer
     #remove it from its current position
-    #potentially array.insert(2, array.delete_at(7))
+    #potential example/model from stack overflow: array.insert(2, array.delete_at(7))
   end
 
 end
@@ -56,5 +56,38 @@ santa.about
 # santa_group << Santa.new("gender queer", "Amazonian")
 
 # p santa_group
+
+#release 4
+class Santa
+  attr_reader :ethnicity
+  attr_accessor :gender, :age
+
+  def initialize(gender, ethnicity)
+    print "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    # @reinder_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
+end
+
+santas = []
+
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+  random_gender = example_genders.sample
+  random_ethnicity = example_ethnicities.sample
+    santa = Santa.new(random_gender, random_ethnicity)
+
+def get_random_age
+  prng = Random.new
+  random_age = prng.rand(140)
+  return random_age
+end
+  santa.age = get_random_age()
+  santas << santa
+
+  p santas
 
 
