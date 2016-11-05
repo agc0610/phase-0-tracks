8bee89416e82634740207f87f18ee918739eddb9
Aimee Gonzalez <agc0610@gmail.com>
2016-11-05 10:22:12 -0700

    create a new hash with nested hashes that contain integers, arrays, and strings as values

---
+++ ruby/nested_data_structures_v2.rb
@@ -0,0 +1,41 @@
+#nested data structure: biology textbook with units that have a name, chapter amounts, names, and starting page numbers
+
+biology_textbook = {
+  unit_1: {
+    "name" => "Basics"
+    "number_of_chapters" => 3
+    "chapter_names" => ["Introduction", "Fundamental Terms and Concepts", "The 6 Kingdoms" ]
+    "starts_on_page" => 10
+  },
+  unit_2: {
+    "name" => "Animals"
+    "number_of_chapters" => 4
+    "chapter_names" => ["Parts of the Animal Kingdom", "Cell Type", "Reproduction and Growth", "Eating"]
+    "starts_on_page" => 130
+  },
+  unit_3: {
+    "name" => "Plants"
+    "number_of_chapters" => 4
+    "chapter_names" => ["Parts of the Plant Kingdom", "Cell Type", "Photosynthesis", "Reproduction"]
+    "starts_on_page" => 220
+  },
+  unit_4: {
+    "name" => "Archaebacteria and Eubacteria"
+    "number_of_chapters" => 5
+    "chapter_names" => ["Parts of the Two Bacteria Kingdoms", "Cell Type", "Making Food", "Cellular Characteristics", "Cellular Activity"]
+    "starts_on_page" => 300
+  },
+  unit_5: {
+    "name" => "Fungi"
+    "number_of_chapters" => 4
+    "chapter_names" => ["Parts of the Fungi Kingdom", "Obtaining Nutrition", "Reproduction", "Cellular Characteristics"]
+    "starts_on_page" => 500
+  },
+  unit_6: {
+    "name" => "Protists"
+    "number_of_chapters" => 3
+    "chapter_names" => ["Parts of the Protist Kingdom", "Obtaining Nutrition and Reproducing", "Cellular Characteristics"]
+    "starts_on_page" => 620
+  }
+}
+
