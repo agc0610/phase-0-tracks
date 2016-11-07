1f015704348e5cfedfcf9e4dcaf4546278673eb4
Aimee Gonzalez <agc0610@gmail.com>
2016-11-06 23:13:18 -0800

    create a user interface to get as many names as user wants to convert, and store in a hash to print when they type quit

--- ruby/alias_manager_v2.rb
+++ ruby/alias_manager_v2.rb
@@ -3,7 +3,6 @@ def switch_first_and_last_name(name)
 end
 
 def spy_name(name)
-  # letter_bank = "AEIOUABCDFGHJKLMNPQRSTVWXYZBaeiouabcdfghjklmnpqrstvwxyzb"
   letter_bank = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
   letter_bank_next = "ECDFIGHJOKLMNPUQRSTVAWXYZBecdfighjoklmnpuqrstvawxyzb"
 
@@ -11,11 +10,36 @@ def spy_name(name)
   for i in 0..name.length - 1
     if letter_bank.count(name[i]) > 0
       letter_index = letter_bank.index(name[i])
-      # name[i] = letter_bank[letter_index + 1]
       name[i] = letter_bank_next[letter_index]
     end
-    end
+  end
     return name
 end
 
-p spy_name("Felicia Torres")  # Vussit Gimodoe
+# p spy_name("Felicia Torres")  # Vussit Gimodoe
+
+def print_names(name_bank)
+  puts name_bank.length.to_s + " names converted: "
+  name_bank.each do |name, new_name|
+    puts "#{name} becomes #{new_name}"
+  end
+end
+
+name_bank = {}
+
+while true do
+  puts "Please provide a name to turn into a spy name: "
+  name = gets.chomp
+
+  if name == "quit"
+    print_names(name_bank)
+    break
+  end
+
+  new_name = spy_name(name)
+
+  puts new_name
+  name_bank[name] = new_name
+
+
+end
