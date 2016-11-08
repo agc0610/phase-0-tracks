cf80ffab365af492e7675b08fb0805833a8b8166
Aimee Gonzalez <agc0610@gmail.com>
2016-11-08 10:41:56 -0800

    add class variables to initialize method, use gender and ethnicity arrays to populate the variable values

--- ruby/santa_v2.rb
+++ ruby/santa_v2.rb
@@ -1,7 +1,11 @@
 class Santa
 
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reinder_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
@@ -14,6 +18,18 @@ class Santa
 
 end
 
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies("chocolate chip")
+# Release 0
+# santa = Santa.new
+# santa.speak
+# santa.eat_milk_and_cookies("chocolate chip")
+
+# Release 1
+santa_group = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Amazonian", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+index = 0
+while index < example_ethnicities.length do
+  santa_group << Santa.new(example_genders[index], example_ethnicities[index])
+  index +=1
+  p santa_group
+end
