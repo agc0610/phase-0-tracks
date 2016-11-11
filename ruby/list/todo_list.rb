6317c44a1f3bbb59c0a4bda6e3d9035b59b55b08
Aimee Gonzalez <agc0610@gmail.com>
2016-11-11 14:38:20 -0800

    write methods to get tests to pass for get items, add item, delete item, get an item's index position

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,34 +1,25 @@
 class TodoList
 
-  def initialize(item1, item2)
-    @item1 = item1
-    @item2 = item2
-    @array = [@item1, @item2]
-    # puts "This is #{@item1} and this is #{@item2}."
+  def initialize(list)
+    @todolist = list
   end
 
   def get_items
-    @array
+    @todolist
   end
 
-  def add_item(new_item)
-    #p @array
-    @new_item = new_item
-    @array << @new_item
-    #p @array
+  def add_item(item)
+    @todolist << item
   end
 
-  def delete_item(item_to_delete)
-    index = @array.index(item_to_delete)
-    @array.delete_at(index)
-    #p @array
+  def delete_item(item)
+    @todolist.delete(item)
   end
 
-  def get_item(index)
-    @array[index]
+  def get_item(num)
+    @todolist[num]
   end
-
 
 end
 
-# driver code "do the dishes", "mow the lawn"
+
