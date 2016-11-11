1de2df7a48149ab120ddb0ed6c5620418ea45040
Aimee Gonzalez <agc0610@gmail.com>
2016-11-10 17:23:07 -0800

    create Puppy class and add methods for speak, roll over, dog years, and shake paws

---
+++ ruby/puppy_methods_v2.rb
@@ -0,0 +1,34 @@
+class Puppy
+
+  def fetch(toy)
+    puts "I brought back the #{toy}!"
+    toy
+  end
+
+  def speak(integer)
+    integer.times do
+      puts "Woof!"
+    end
+  end
+
+  def roll_over
+  puts "*rolls over*"
+  end
+
+  def dog_years(integer)
+  dog_age = integer * 7
+  dog_age
+  end
+
+  def shake_paws
+    puts "*shakes paws*"
+  end
+
+end
+
+puppy = Puppy.new
+puppy.fetch("Ball")
+puppy.speak(3)
+puppy.roll_over
+puppy.dog_years(2)
+puppy.shake_paws
